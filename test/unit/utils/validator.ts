df0eaba1b43adf8062ca5d3923f585e8a1f21cc5
Jialiang@iMac <jailiang.chang@wmich.edu>
2020-04-14 13:04:45 -0400

    Add unit test and sign commits

--- test/unit/utils/validator.ts
+++ test/unit/utils/validator.ts
@@ -1,14 +1,10 @@
 import should from 'should';
 import * as Validator from '../../../src/utils/validate';
 import { HashType } from '../../../src/coin/baseCoin/iface';
+import { hashTypes } from '../../../src/coin/xtz/utils';
 
 describe('Shared utils validate library should', function() {
-  // arbitrary text
-  const arr = [127, 255, 31, 192, 3, 126, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   const hex = '7FFF1FC0037E0000000000000000000000000000000000000000000000000000';
-  const txt = 'arbitrary string to sign';
-  const signedString =
-    '0x9424113f32c17b6ffbeee024e1a54b6991d756e82f66cca16a41231fdfa270d03b08e833f5dbbd5cc86896c2e5ea6c74d2e292cda21f717164f994fcdf28486d1b';
 
   it('validate a hex string', () => {
     const hex = ['0xaffd', '0x11'];
@@ -20,5 +16,10 @@ describe('Shared utils validate library should', function() {
     invalidHex.map(hex => {
       should(Validator.isValidHex(hex)).equal(false);
     });
+  });
+
+  it('validate a hash', () => {
+    const hash = 'spsig1DWTuXdgUg2t64PLRfaapsYejCoCVVkqy2Zjv41Zirt7MjoqSfBnP38qoAg3SWicfQNiG25yMqGYge4jrfrwv9H8hRKDyY';
+    Validator.isValidHash(hash, hashTypes.spsig1).should.be.true();
   });
 });
